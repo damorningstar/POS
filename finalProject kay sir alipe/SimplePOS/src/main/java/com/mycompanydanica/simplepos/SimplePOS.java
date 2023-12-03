a0f40fc6aa81a35c1c3d160240ad7bab3330e4b1
damorningstar <152599870+damorningstar@users.noreply.github.com>
2023-12-03 17:40:35 +0800

    Add files via upload

---
+++ finalProject kay sir alipe/SimplePOS/src/main/java/com/mycompanydanica/simplepos/SimplePOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompanydanica.simplepos;
+
+/**
+ *
+ * @author Hi
+ */
+public class SimplePOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
